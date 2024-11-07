8a376898e02e299847e93a9ab1ea280c735ae7ed
duminghong <i@duminghong.com>
2024-11-07 10:40:42 +0800

    🔧 build: build修改

--- .vitepress/theme/data/features.js
+++ .vitepress/theme/data/features.js
@@ -1,26 +1,26 @@
 export default [
   {
-    "title": "小程序canvas文本绘制",
-    "details": "优化原理：通过基础库 1.9.90 开始支持的measureText接口返回的文本宽度，把每个字符宽度不断累加，精确计算在哪个位置应该换行去实现这个功能。",
-    "link": "/courses/小程序canvas文本绘制",
-    "firstImage": "../assets/img/4853363-229d689d5f48c183.jpg",
-    "time": "2024-11-03 16:10:48",
-    "source": "courses"
+    "title": "音频可视化",
+    "details": "包含各个 AudioNode 对象以及它们的联系的对象，可以理解为 audio 上下文对象。（类似于 canvas 的 getContext()）\n绝大多数情况下，一个 document 中只能有一个 AudioContext",
+    "link": "/notes/音频可视化",
+    "firstImage": null,
+    "time": "2024-11-06 21:03:01",
+    "source": "notes"
   },
   {
-    "title": "小程序封面-七牛图片裁剪",
-    "details": "使用七牛的 图片高级处理(imageMogr2 ) 接口去实现",
-    "link": "/courses/小程序封面-七牛图片裁剪",
-    "firstImage": "../assets/img/4853363-ffe43d1e723ffe35.png",
-    "time": "2024-11-03 16:10:48",
-    "source": "courses"
+    "title": "小程序爬坑",
+    "details": "需要先 wx.login() ，再 wx.getUserInfo()\n  @讲个笑话遗忘 ： sessionKey 存在于 redis 缓存，登录会刷新缓存，顺序颠倒可能会解密失败",
+    "link": "/notes/小程序爬坑",
+    "firstImage": "../assets/img/4853363-eec24a259212a291.png",
+    "time": "2024-11-06 21:03:01",
+    "source": "notes"
   },
   {
-    "title": "聊天框实现图片表情连续插入",
-    "details": "直接显示表情，且可以在文字中间插入表情，包括换行显示。",
-    "link": "/courses/聊天框实现图片表情连续插入",
-    "firstImage": "../assets/img/4853363-175b182eafb36d0b.gif",
-    "time": "2024-11-03 16:10:48",
-    "source": "courses"
+    "title": "富文本编辑器爬坑",
+    "details": "我们项目需要视频，音频，定位地址，我直接使用&lt;iframe&gt; 嵌套页面，例如音频：",
+    "link": "/notes/富文本编辑器爬坑",
+    "firstImage": null,
+    "time": "2024-11-06 21:03:01",
+    "source": "notes"
   }
 ]
