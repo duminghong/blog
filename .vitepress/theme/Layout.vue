d6894a3b5e6f972824e8e652e5051b180b66e9cd
duminghong <i@duminghong.com>
2025-10-16 09:21:16 +0800

    feat(theme): 添加返回顶部按钮并优化代码格式
    在主题布局中添加 el-backtop 组件实现返回顶部功能
    同时调整脚本部分的代码格式，移除多余空行

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -3,17 +3,18 @@
     <template #doc-after>
       <!-- 首页 Layout 使用的是 doc 所以需要判断，如果是 page 不会使用插槽 -->
       <Comment v-if="!isHome" :key="page.filePath"></Comment>
+      <el-backtop :right="100" :bottom="100" />
     </template>
   </Layout>
 </template>
-  
+
 <script setup>
-  import { unref, computed } from 'vue';
-  import { useData } from 'vitepress';
-  import DefaultTheme from 'vitepress/theme';
-  import Comment from './components/Comment.vue';
-  
-  const { page } = useData();
-  const { Layout } = DefaultTheme;
-  const isHome = computed(() => unref(page)?.filePath.indexOf('index.md') != -1 || unref(page)?.filePath.indexOf('about.md') != -1);
+import { unref, computed } from 'vue';
+import { useData } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import Comment from './components/Comment.vue';
+
+const { page } = useData();
+const { Layout } = DefaultTheme;
+const isHome = computed(() => unref(page)?.filePath.indexOf('index.md') != -1 || unref(page)?.filePath.indexOf('about.md') != -1);
 </script>
